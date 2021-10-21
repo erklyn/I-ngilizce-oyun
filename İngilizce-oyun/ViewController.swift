2293f636ce8692b9d65d8004fa5c6d416afb2f75
Utku Enes GÜRSEL <65543461+erklyn@users.noreply.github.com>
2021-10-21 16:59:22 +0300

    basic layout of the landing page

--- İngilizce-oyun/ViewController.swift
+++ İngilizce-oyun/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var chooseYourTest: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func testChosen(_ sender: UIButton) {
+        //Hangi oyunun seçildiğini öğren
+        var choosenTest = sender.currentTitle!
+        
+    
+        // Türkçeden İngilizce Seçilirse türkçeden ingilizceye olan oyuna gönder
+        if choosenTest == "İngilizceden Türkçeye"{
+            
+        }else{
+            // İngilizceden Türkçe Seçilirse o oyuna gönder
+            
+        }
+        
+        
+        
+    }
+    
 }
 
